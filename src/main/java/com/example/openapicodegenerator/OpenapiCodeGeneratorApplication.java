4f57aeee36192d370dc3d2f7c4532a7c50c79706
JoisFe <rhsnqk@gmail.com>
2023-08-26 23:29:30 +0900

    openAPI and Code Generator Practice Project Start

---
+++ src/main/java/com/example/openapicodegenerator/OpenapiCodeGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.openapicodegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenapiCodeGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenapiCodeGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/openapicodegenerator/OpenapiCodeGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.openapicodegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenapiCodeGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
